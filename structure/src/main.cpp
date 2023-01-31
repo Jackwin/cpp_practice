d60765c8ad0d86e929da4ec26b865664d7b30916
Jackwin <rcwcj@126.com>
2023-01-31 17:41:30 +0800

    featt: update struct

--- structure/src/main.cpp
+++ structure/src/main.cpp
@@ -1,30 +1,42 @@
 
 #include <iostream>
+
 #include "struct_example.hpp"
 
 int main() {
-//   DataGeneratorParam::DataGeneratorParam(int num) {
-// 	  for(int i = 0; i < num; i++) {
-// 	  cout << ""The data genearated is "" << i <<endl;
-// }
-// DataGeneratorParam::DataGeneratorParam(8);
-	std::string my_name = "Jack";
-	std::string friend_name = "Jane";
-	std::string city = "Beijing";
-	// Construct the structure with parameters
-	Person jack(10, 20, my_name, friend_name, city);
-	jack.printName();
-	int x = jack.getAge();
-	std::cout << "The age of Jack is " << x << std::endl;
-	jack.greet();
+  //   DataGeneratorParam::DataGeneratorParam(int num) {
+  // 	  for(int i = 0; i < num; i++) {
+  // 	  cout << ""The data genearated is "" << i <<endl;
+  // }
+  // DataGeneratorParam::DataGeneratorParam(8);
+  std::string my_name = "Jack";
+  std::string friend_name = "Jane";
+  std::string city = "Beijing";
 
-	// Constuct the structure without parameters
-	Person jim = {11, 21, "Jim", "Tom", "Shanghai"};
-	int jim_age = jim.getAge();
-	std::cout <<  "The age of jim is " << x << std::endl;
-	jim.greet();
+  CoffeeMachine coffee_machine;
+  coffee_machine.operator_name = "Jack";
+  coffee_machine.coffee_type = "Latte";
+  coffee_machine.func = make_coffee;
+  coffee_machine.func(&coffee_machine);
+  coffee_machine.func = warm_milk;
+  coffee_machine.func(&coffee_machine);
 
-	hello hi;
-	hi.print();
-	return 0;
+  // Construct the structure with parameters
+  /*
+  Person jack(10, 20, my_name, friend_name, city);
+  jack.printName();
+  int x = jack.getAge();
+  std::cout << "The age of Jack is " << x << std::endl;
+  jack.greet();
+
+  // Constuct the structure without parameters
+  Person jim = {11, 21, "Jim", "Tom", "Shanghai"};
+  int jim_age = jim.getAge();
+  std::cout <<  "The age of jim is " << x << std::endl;
+  jim.greet();
+
+  hello hi;
+  hi.print();
+  return 0;
+  */
 }
